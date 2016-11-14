e850e2030e780044537bb85f2558e6957445ada1
Robins Gupta <robinskumar73@gmail.com>
2016-11-14 18:07:47 +0530

    first commit

---
+++ client/scripts/controllers/detailView.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module($snaphy.getModuleName())
+
+//Controller for detailViewControl ..
+.controller('detailViewControl', ['$scope', '$stateParams', 'Database',
+    function($scope, $stateParams, Database) {
+        //Checking if default templating feature is enabled..
+        var defaultTemplate = $snaphy.loadSettings('detailView', "defaultTemplate");
+        $snaphy.setDefaultTemplate(defaultTemplate);
+        //Use Database.getDb(pluginName, PluginDatabaseName) to get the Database Resource.
+    }//controller function..
+]);
