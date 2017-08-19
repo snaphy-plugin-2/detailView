e41771d399e8ca39ab5fec5bcaa8d1ee84bd8617
Robins Gupta <robinskumar73@gmail.com>
2017-08-19 17:51:39 +0530

    Code modified.

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@
 
 module.exports = function( server, databaseObj, helper, packageObj) {
 	const _ =  require("lodash");
+	const automata = helper.loadPlugin("automata");
 	/**
 	 * Here server is the main app object
 	 * databaseObj is the mapped database from the package.json file
@@ -62,13 +63,37 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 
 		modelObj.getDetailSchema = function(callback) {
 			//Now form the schema and send it to the client..
-			let relations = modelObj.definition.settings.relations;
-			//Get template structure..
-			let schema = generateTemplateStr(app, modelName);
+            let relations = modelObj.definition.settings.relations,
+                settings;
 
-			//Now recursively add relations to the models...
-			addNestedModelRelation(app, schema, relations, modelName);
-			callback(null, schema);
+            //Load login plugin get roles method..
+            const {getAuthorisedRoles} = helper.loadPlugin('login');
+            //Find the authorised roles for the data..
+            if(getAuthorisedRoles) {
+                getAuthorisedRoles(server, function (err, roleList) {
+                    if (err) {
+                        console.error("Error occured in fetching roles.", err);
+                        callback(err);
+                    } else {
+                        automata.getSnaphyACL(modelName, roleList)
+                            .then(function (snaphyACL) {
+                                return generateSchemaByACL(app, modelName, relations, settings, roleList, snaphyACL);
+                            })
+                            .then(function (schema) {
+                                callback(null, schema);
+                            })
+                            .catch(function (error) {
+                                callback(error);
+                            });
+                    }
+                });
+            }else{
+                let err = new Error("getAuthorisedRoles is not defined in login plugin");
+                callback(err, null);
+            }
+
+
+
 		};
 
 		//Now registering the method `getAbsoluteSchema` required for robust automata plugin..
@@ -80,6 +105,72 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 			}
 		);
 	};
+
+
+
+    /**
+     * Generate Schema according ACL and defined roles.
+     * @param app
+     * @param modelName
+     * @param relations
+     * @param settings
+     * @param roleList
+     * @param snaphyACL
+     */
+    var generateSchemaByACL = function(app, modelName, relations, settings, roleList, snaphyACL){
+        return new Promise(function (resolve, reject) {
+            const tableObj = helper.getTableJson(modelName);
+            if(tableObj){
+
+                tableObj.settings = tableObj.settings || {};
+                if(tableObj.settings){
+                    settings = tableObj.settings;
+                    if(snaphyACL){
+                        //Check for read permission..
+                        if(snaphyACL.read === "deny"){
+                            settings.read = false;
+                        }else if(snaphyACL.read === "allow"){
+                            //handle setting for read === true..
+                            settings.read = true;
+                        }
+
+                        //Check for read permission..
+                        if(snaphyACL.create === "deny"){
+                            settings.create = false;
+                        }else if(snaphyACL.create === "allow"){
+                            //handle setting for read === true..
+                            settings.create = true;
+                        }
+
+                        //Check for read permission..
+                        if(snaphyACL.edit === "deny"){
+                            settings.edit = false;
+                        }else if(snaphyACL.edit === "allow"){
+                            //handle setting for read === true..
+                            settings.edit = true;
+                        }
+
+                        //Check for read permission..
+                        if(snaphyACL.delete === "deny"){
+                            settings.delete = false;
+                        }else if(snaphyACL.delete === "allow"){
+                            //handle setting for read === true..
+                            settings.delete = true;
+                        }
+                    }
+                }
+            }
+
+            //Get template structure..
+            let schema = generateTemplateStr(app, modelName,  null, roleList, snaphyACL);
+
+            //Now recursively add relations to the models...
+            addNestedModelRelation(app, schema, relations, modelName, roleList, snaphyACL);
+
+            schema.settings  = settings;
+            resolve(schema);
+        });
+    };
 
 
 	var extend = function(original, context, key) {
@@ -104,14 +195,25 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 	 * @param schema
 	 * @param relations
 	 * @param rootModelName model name of the root
-	 */
-	var addNestedModelRelation = function(app, schema, relations, rootModelName){
+     * @param roleList {[string]} list of roles assigned to current logged user..
+     * @param snaphyACL
+     */
+	var addNestedModelRelation = function(app, schema, relations, rootModelName, roleList, snaphyACL){
 		//Now adding  prop of belongTo and hasMany method to the header and schema respectfully...
 		for(var relationName in relations){
 			if(relations.hasOwnProperty(relationName)){
 				var relationObj = {};
 					relationObj = extend({}, relations[relationName]);
 				var modelName       = relationObj.model;
+
+                //Flag to track if to reject property or accept prop..
+                var rejectProperty = automata.checkPropertyAccess(relationObj, roleList, "relations");
+                var dynamicRejectProperty = automata.checkDynamicPropertyAccess(relationName, '', roleList, snaphyACL, "relations");
+                if(rejectProperty || dynamicRejectProperty){
+                    //Skip this property..
+                    continue;
+                }
+
 				if((relationObj.type === 'hasOne' || relationObj.type === 'belongsTo') && relationObj.templateOptions !== undefined){
 
 					if(relationObj.type === "hasOne"){
@@ -130,7 +232,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 					belongsToSchema.templateOptions.model      = relationObj.model;
 					belongsToSchema.templateOptions.foreignKey = relationObj.foreignKey === "" ? relationName + 'Id' : relationObj.foreignKey;
 					//Now add nested schema to the relational model.
-					generateTemplateStr(app, relationObj.model, belongsToSchema.templateOptions);
+					generateTemplateStr(app, relationObj.model, belongsToSchema.templateOptions, roleList);
 
 					if(belongsToSchema.templateOptions.includeRelatedModel){
 						//Now if model-> related model -> related model (belongTo data is requested)
@@ -151,17 +253,21 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 					}
 
 					//add schema
-					addNestedModelRelation(app, belongsToSchema.templateOptions, relatedModelRelations, relationObj.model);
+					addNestedModelRelation(app, belongsToSchema.templateOptions, relatedModelRelations, relationObj.model, roleList, snaphyACL);
+                    const checkRelationEditAccess_ = automata.checkRelationEditAccess(relationObj, roleList);
+                    //Only allow if reject value is set to be false..
+                    if(!checkRelationEditAccess_){
+                        if(relations[relationName].templateOptions.container){
+                            schema.container[relations[relationName].templateOptions.container] = schema.container[relations[relationName].templateOptions.container] || automata.initializeContainer();
+                            schema.container[relations[relationName].templateOptions.container].schema.push(belongsToSchema);
+                        }else{
+                            //Now add this to the schema..
+                            schema.container.default = schema.container.default || automata.initializeContainer();
+                            schema.container.default.schema.push(belongsToSchema);
+                            //schema.fields.push(belongsToSchema);
+                        }
+                    }
 
-					if(relations[relationName].templateOptions.container){
-						schema.container[relations[relationName].templateOptions.container] = schema.container[relations[relationName].templateOptions.container] || initializeContainer();;
-						schema.container[relations[relationName].templateOptions.container].schema.push(belongsToSchema);
-					}else{
-						//Now add this to the schema..
-						schema.container.default = schema.container.default || initializeContainer();
-						schema.container.default.schema.push(belongsToSchema);
-						//schema.fields.push(belongsToSchema);
-					}
 
 
 
@@ -292,45 +398,6 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 	};
 
 
-    /**
-     *  Intiailize the container object for a container type
-     * @returns {{class: Array, style: {}, schema: Array, options: {}}}
-     */
-	const initializeContainer = function () {
-		return {
-            //Class in form or array..
-		    class: [],
-            style:{},
-            //Store schema for the given container
-            schema:[],
-            //For storing additional options..
-            options:{}
-		};
-    };
-
-    /**
-     * Add container definition like class or style..
-     * @param schema
-     * @param tableObj
-     */
-	const addContainerSettings = function (schema, tableObj) {
-	    if(tableObj.container){
-	        for(const containerName in tableObj.container){
-	            if(tableObj.container.hasOwnProperty(containerName)){
-                    //initializeContainer();
-                    schema.container[containerName] = schema.container[containerName] || initializeContainer();
-                    let settings = tableObj.container[containerName];
-                    //Assign properties value to schema..
-                    for(const prop in settings){
-                        if(settings.hasOwnProperty(prop)){
-                            schema.container[containerName][prop] = settings[prop];
-                        }
-                    }
-                }
-            }
-        }
-        return schema;
-    };
 
 
 	/**
@@ -338,10 +405,12 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 	 * @param app
 	 * @param modelName
 	 * @param schema {Object} optional
+     * @param roleList {[string]} list of roles for current logged user. Loopback current context.
+     * @param snaphyACL
 	 * @returns {*}
 	 */
-	const generateTemplateStr = function(app, modelName, schema){
-		if(schema === undefined){
+	const generateTemplateStr = function(app, modelName, schema, roleList, snaphyACL){
+		if(!schema){
 			schema = {};
 			schema.model = modelName;
 			schema.relations = {
@@ -352,8 +421,9 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 		//Start adding container..
 		schema.container = {};
 		//Store different fields by their name,,
-		schema.container.default   = schema.container.default || initializeContainer();
+		schema.container.default   = schema.container.default || automata.initializeContainer();
 		const validationModelObj = helper.getValidationObj(modelName);
+
 		//path of table data from //common/table/model.json data
 		const tableModelObj = helper.getTablePath(modelName);
 		//Actual table object data..
@@ -366,7 +436,7 @@ module.exports = function( server, databaseObj, helper, packageObj) {
         }
 
         //Add container settings stored in table definitions..file..
-        addContainerSettings(schema, tableModelData);
+        automata.addContainerSettings(schema, tableModelData);
 
 		let
 			validationObj,
@@ -396,37 +466,45 @@ module.exports = function( server, databaseObj, helper, packageObj) {
 			if(modelProperties.hasOwnProperty(propertyName)){
 				let propObj = modelProperties[propertyName].template;
 				if(propObj !== undefined){
-					propObj.key = propertyName;
-					//also add the validation to the object..
-					try{
-						var validationRules = validationObj.rules[propertyName];
-						var validationMessages = validationObj.messages[propertyName];
+                    //Flag to track if to reject property or accept prop..
+                    let rejectProperty = automata.checkPropertyAccess(propObj, roleList, "fields");
+                    let dynamicRejectProperty = automata.checkDynamicPropertyAccess(propertyName, '', roleList, snaphyACL, "fields");
 
-						if(propObj.templateOptions && validationRules){
-							if(propObj.templateOptions.id){
-								var validationName = propObj.templateOptions.id;
-								//Get the validation object..
-								newValidationObj.rules[validationName] = validationRules;
-								newValidationObj.messages[validationName] = validationMessages;
-							}
-						}
-					}catch(err){
-						// Do nothing
-						// Validation is not defined in the model definition
-					}
 
-					if(propObj.templateOptions){
-						if(propObj.templateOptions.container){
-							schema.container[propObj.templateOptions.container] = schema.container[propObj.templateOptions.container] || initializeContainer();
-							schema.container[propObj.templateOptions.container].schema.push(propObj);
-						}else{
-							schema.container.default   = schema.container.default || initializeContainer();
-							schema.container.default.schema.push(propObj);
-						}
-					}else{
-						schema.container.default   = schema.container.default || initializeContainer();
-						schema.container.default.schema.push(propObj);
-					}
+                    //Add property only if rejectProperty value is false..
+                    if(!rejectProperty && !dynamicRejectProperty) {
+                        propObj.key = propertyName;
+                        //also add the validation to the object..
+                        try {
+                            var validationRules = validationObj.rules[propertyName];
+                            var validationMessages = validationObj.messages[propertyName];
+
+                            if (propObj.templateOptions && validationRules) {
+                                if (propObj.templateOptions.id) {
+                                    var validationName = propObj.templateOptions.id;
+                                    //Get the validation object..
+                                    newValidationObj.rules[validationName] = validationRules;
+                                    newValidationObj.messages[validationName] = validationMessages;
+                                }
+                            }
+                        } catch (err) {
+                            // Do nothing
+                            // Validation is not defined in the model definition
+                        }
+
+                        if (propObj.templateOptions) {
+                            if (propObj.templateOptions.container) {
+                                schema.container[propObj.templateOptions.container] = schema.container[propObj.templateOptions.container] || automata.initializeContainer();
+                                schema.container[propObj.templateOptions.container].schema.push(propObj);
+                            } else {
+                                schema.container.default = schema.container.default || automata.initializeContainer();
+                                schema.container.default.schema.push(propObj);
+                            }
+                        } else {
+                            schema.container.default = schema.container.default || automata.initializeContainer();
+                            schema.container.default.schema.push(propObj);
+                        }
+                    }//If rejectProperty and dynamicProperty
 
 				}
 			}
